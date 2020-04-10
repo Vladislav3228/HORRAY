4298570e32677d6068adafb7579acedb73e06864
Vladislav <33334984+Vladislav3228@users.noreply.github.com>
2020-04-10 17:57:13 +0800

    Created using Colaboratory

---
+++ 3laba.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "3laba",
+      "provenance": [],
+      "collapsed_sections": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "display_name": "Python 3",
+      "name": "python3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Vladislav3228/HORRAY/blob/master/3laba.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "colab_type": "code",
+        "id": "C4HZx7Gndbrh",
+        "colab": {}
+      },
+      "source": [
+        "print('ti')\n",
+        "      "
+      ],
+      "execution_count": 0,
+      "outputs": []
+    }
+  ]
+}
